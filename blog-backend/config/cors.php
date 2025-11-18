fd450d59d424cf4282f5d694703f188102889f8a
Ursule <aizannonursule@gmail.com>
2025-11-18 09:12:20 +0100

    mise

--- blog-backend/config/cors.php
+++ blog-backend/config/cors.php
@@ -3,8 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://blog-ursule-e2u7.vercel.app',
+    ],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
-
